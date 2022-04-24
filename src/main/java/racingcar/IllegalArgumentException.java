c9f5648cb8e39c444f462d5776c5da97af3bee03
bestsilver <dkvkxm99@gmail.com>
2022-04-25 00:47:28 +0900

    refactor: IllegalArgumentException
    - exception 패키지로 이동
    - 자동차 이름 최대값 초과에 따른 에러 상수화
    - 자동차 이름 최소값 미만에 따른 에러 상수화
    - 시도 횟수값 숫자가 아닐 경우 에러 상수화
    - 시도 횟수값 최소값 미만일 경우 에러 상수화

--- src/main/java/racingcar/IllegalArgumentException.java
+++
@@ -1,9 +0,0 @@
-package racingcar;
-
-public class IllegalArgumentException extends RuntimeException{
-    public static final String MAX_LENGTH_CAR_NAME_ERROR_MSG = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
-    public static final String MIN_LENGTH_CAR_NAME_ERROR_MSG = "[ERROR] 자동차 이름은 1자 이상이여야 합니다.";
-    public static final String NOT_NUMBER_ERROR_MSG = "[ERROR] 시도 횟수는 숫자만 입렵할 수 있습니다.";
-
-    public IllegalArgumentException(final String errorMessage) {super(errorMessage);}
-}
